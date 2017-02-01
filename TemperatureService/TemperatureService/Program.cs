a04cef15a68925ddf8d3d80681aebf1bcdd42164
María Alicia de Andrés Herrero <deandresherrero@gmail.com>
2017-02-01 12:04:20 +0100

    First Commit ServiceTemperature

---
+++ TemperatureService/TemperatureService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureService
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting temperature: ");
+            TemperatureService tempServ = new TemperatureService();
+            tempServ.getTemperature();
+            Console.WriteLine("The temprature is: {0}" , tempServ.getTemperature()); 
+
+        }
+    }
+}
---
+++ TemperatureService/TemperatureService/TemperatureService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+
+namespace TemperatureService
+{
+    public class TemperatureService: ITemperatureService
+    {
+        public string getTemperature()
+        {
+            XDocument output = new XDocument();
+            HttpWebRequest http = (HttpWebRequest)WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=[LOCATION VALUE]&appid=5a4e086cd72628bca4015adbe80aecad&units=metric");
+            WebResponse response = http.GetResponse();
+
+
+            Stream stream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string content = sr.ReadToEnd();
+
+            //I could use better XDocument or JSonDeserializer
+
+            return content;
+        }
+    }
+}
